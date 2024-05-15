6920ee70884d2d16989a3ef335f386d3cc7a4a65
Luis Ruiz <luisruimore@gmail.com>
2024-05-16 01:32:32 +0200

    My cripto simulation
    Una simulación de mi propia criptomoneda
    Falta README.md

---
+++ LuisCoin5002.py
@@ -0,0 +1,197 @@
+import datetime
+import hashlib  # para poder hashear los bloques
+import json  # codificar antes de hashearlo
+from flask import Flask, jsonify, request
+import requests
+from uuid import uuid4
+from urllib.parse import urlparse
+
+
+# Paso 1 - Crear nuestra blockchain
+class Blockchain:
+    def __init__(self):
+        self.chain = []  # Al inicio nuestra cadena será una lista vacia
+        self.transactions = []
+        self.create_block(proof=1, previous_hash='0')  # '' porque usaremos SHA256 que solo acepta comilla simple
+        self.nodes = set()
+
+    def create_block(self, proof, previous_hash):
+        block = {'index': len(self.chain) + 1,
+                 'timestamp': str(datetime.datetime.now()),  # Tiempo de cuando se crea el bloque
+                 'proof': proof,
+                 'previous_hash': previous_hash,
+                 'transactions': self.transactions
+                 }
+        self.transactions = []
+        self.chain.append(block)  # Adjuntamos el bloque a nuestra cadena
+        return block
+
+    def add_transaction(self, sender, receiver, amount):
+        self.transactions.append({
+            'sender': sender,
+            'reciever': receiver,
+            'amount': amount
+        })
+        previous_block = self.get_previous_block()
+        return previous_block['index'] + 1
+
+    # Cada nodo entrará a partir de un puerto de nuestra app web de Flask
+    def add_node(self, address):
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)  # netloc es el url
+
+    def replace_chain(self):
+        network = self.nodes
+        longest_chain = None
+        max_length = len(self.chain)
+
+        for node in network:
+            response = requests.get(f'http://{node}/get_chain')
+            if response.status_code == 200:
+                length = response.json().get('length')
+                chain = response.json().get('chain')
+
+                if length > max_length and self.is_chain_valid(chain):
+                    max_length = length
+                    longest_chain = chain
+            if longest_chain:
+                self.chain = longest_chain
+                return True
+            return False
+
+    def get_previous_block(self):
+        return self.chain[-1]  # Usamos -1 para usar el último bloque
+
+    # Un problema dificil de resolver y facil de verificar
+
+    def proof_of_work(self, previous_proof):
+        new_proof = 1
+        check_proof = False
+
+        while check_proof is False:  # Hasta que encontremos el proof que resuelva el problema
+            hash_operation = hashlib.sha256(str(new_proof ** 2 - previous_proof ** 2).encode()).hexdigest()
+            if hash_operation[:4] == '0000':
+                check_proof = True
+            else:
+                new_proof += 1
+        return new_proof
+
+    # Debemos checkear el previous hash de la cadena
+    def hash(self, block):
+        encoded_block = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(encoded_block).hexdigest()
+
+    def is_chain_valid(self, chain):
+        previous_block = chain[0]
+        block_index = 1
+        while block_index < len(chain):  # iterar sobre todos los bloques
+            block = chain[block_index]
+            if block['previous_hash'] != self.hash(previous_block):
+                return False
+            previous_proof = previous_block['proof']
+            proof = block['proof']
+            hash_operation = hashlib.sha256(str(proof ** 2 - previous_proof ** 2).encode()).hexdigest()
+            if hash_operation[:4] != '0000':
+                return False
+
+            previous_block = block
+            block_index += 1
+        return True
+
+
+# Web App para interactuar con nuestra blockchain
+app = Flask(__name__)  # estandar de flask
+blockchain = Blockchain()
+
+# Creando una direccion para el nodo
+node_address = str(uuid4()).replace('-', '')  # Direccion unica aleatoria
+
+
+# Minar un Nuevo Bloque
+@app.route('/mine_block', methods=['GET'])
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+
+    proof = blockchain.proof_of_work(previous_proof)
+
+    previous_hash = blockchain.hash(previous_block)
+    blockchain.add_transaction(sender=node_address, receiver='Adrian', amount=1)
+    block = blockchain.create_block(proof, previous_hash)
+
+    response = {
+        'message': 'Felicidades has minado un bloque!',
+        'index': block['index'],
+        'timestamp': block['timestamp'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+        'transactions': block['transactions']
+    }
+
+    return jsonify(response), 200
+
+
+# Obtener toda la blockchain
+@app.route('/get_chain', methods=['GET'])
+def get_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain)
+    }
+    return jsonify(response), 200
+
+
+# Checkear la validez de la cadena de bloques
+@app.route("/is_valid", methods=['GET'])
+def is_valid():
+    is_valid = blockchain.is_chain_valid(blockchain.chain)
+    if is_valid:
+        response = {'message': 'El blockchain es valido'}
+    else:
+        response = {'meessage': 'El blockchain ES INVALIDO!!!!!'}
+    return jsonify(response), 200
+
+
+@app.route("/new_transaction", methods=['POST'])
+def new_transaction():
+    json = request.get_json()
+    transaction_keys = ['sender', 'receiver', 'amount']
+    if not all(key in json for key in transaction_keys):
+        return 'Algunos elementos de la transaccion estan faltando', 400
+    index = blockchain.add_transaction(json['sender'], json['receiver'], json['amount'])
+    response = {'message': f'Esta transaccion será añadida al bloque {index}'}
+    return jsonify(response), 201
+
+
+# Paso 3 - Descentralizar nuestra blockchain
+# Conectar nuevos nodos
+@app.route('/connect_node', methods=['POST'])
+def connect_node():
+    json = request.get_json()
+    nodes = json.get('nodes')
+
+    if nodes is None:
+        return 'No hay nodos', 400
+    for node in nodes:
+        blockchain.add_node(node)
+    response = {
+        'message': 'Todos los nodos estan conectados. La LuisCoin Blockchain contiene los siguientes nodos:',
+        'total_nodes': list(blockchain.nodes)
+    }
+    return jsonify(response), 201
+
+
+# Reemplazar la cadena por la más larga
+@app.route('/replace_chain', methods=['GET'])
+def replace_chain():
+    is_chain_replaced = blockchain.replace_chain()
+    if is_chain_replaced:
+        response = {'message': 'La cadena ha sido reemplazada por la más larga',
+                    'new_chain': blockchain.chain}
+    else:
+        response = {'message': 'Esta cadena es la más larga',
+                    'actual_chain': blockchain.chain}
+    return jsonify(response), 200
+
+
+app.run(host='0.0.0.0', port=5002)
